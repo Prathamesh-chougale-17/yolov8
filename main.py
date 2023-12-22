b68003d4cf6cc96de16cdb6c841f61e4a2ea1b82
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-22 14:20:48 +0530

    Update config.yaml and main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from ultralytics import YOLO
 
+# from ultralytics.yolo.v8.detect.predict import Detection
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1)
+# results = model.train(data="config.yaml", epochs=1)
 
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+
+result = model.predict(source="0", show=True)
+
+print(result)
