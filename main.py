9da9dc39958b95b6db2ea71693c0f32b4abed45a
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-01-05 15:03:50 +0530

    Fix model training and prediction code

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ model = YOLO("yolov8n.pt")  # build a new model from scratch
 # import cv2
 
 # Use the model
-# results = model.train(data="config.yaml", epochs=1)
+results = model.train(data="config.yaml", epochs=1)
 
 # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 
-results = model.predict(source="0", show=True, stream=True)
-for r in results:
-    boxes = r.boxes  # Boxes object for bbox outputs
-    masks = r.masks  # Masks object for segment masks outputs
-    probs = r.probs  # Class probabilities for classification outputs
+# results = model.predict(source="0", show=True, stream=True)
+# for r in results:
+#     boxes = r.boxes  # Boxes object for bbox outputs
+#     masks = r.masks  # Masks object for segment masks outputs
+#     probs = r.probs  # Class probabilities for classification outputs
 
 print(results)
